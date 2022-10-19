33fbfa394472c55870ac02b57854131a3ebe4489
joebywan <joebywan@users.noreply.github.com>
2022-10-20 09:04:41 +1000

    Update deploy.tf

--- ec2_spot_module/example/deploy.tf
+++ ec2_spot_module/example/deploy.tf
@@ -4,25 +4,23 @@ module "example" {
   security_group_rules = [
     {
       port = 80
-      cidr = 0.0.0.0/0
+      cidr = "0.0.0.0/0"
     },
     {
       port = 443
-      cidr = 0.0.0.0/0
+      cidr = "0.0.0.0/0"
     }
   ]
-  ec2_key = "testSSHkey"
-  subnet_id = "id here"
-  vpc_id = "id here"
-  naming_prefix = "text to put at start of names"
-  tags = {
-    owner = "Dr Seuss"
-  }
-  operating_system = "AMAZONLINUX2" # Currently supports AMAZONLINUX2 & UBUNTU
-  userdata = "./stuff.sh" # Optional
-  root_drive_size = 20 # Optional, defaults to 8
-  instance_type = "t3.xlarge" # Optional, defaults to t2.micro
-  instance_profile = "AmazonSSMRoleForInstancesQuickSetup"
-  spot_type = "one-time" # Optional, defaults to one-time, otherwise can use persistent
-  spot_price = 0.05 # Only required if spot = true.  Price to bid up to for instance
+  ec2_key = var.ec2_key
+  subnet_id = var.subnet_id
+  vpc_id = var.vpc_id
+  naming_prefix = var.naming_prefix
+  tags = var.tags
+  operating_system = var.operating_system # Currently supports AMAZONLINUX2 & UBUNTU
+  userdata = var.userdata # Optional
+  root_drive_size = var.root_drive_size # Optional, defaults to 8
+  instance_type = var.instance_type # Optional, defaults to t2.micro
+  instance_profile = var.instance_profile
+  spot_type = var.spot_type # Optional, defaults to one-time, otherwise can use persistent
+  spot_price = var.spot_price # Only required if spot = true.  Price to bid up to for instance
 }
